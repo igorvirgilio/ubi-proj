dab1e4e8fe35093c987a85b2d64bd2a55f9a39d8
Igor Virgílio <igor.virgilio@gmail.com>
2020-04-18 17:01:32 +0100

    update DB settings

--- ubi_proj/settings.py
+++ ubi_proj/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'ubiwhere',
+        'NAME': 'ubi_proj',
         'USER': 'ubi_user',
         'PASSWORD': 'ubi@123',
         'HOST': 'ubi_db',
